1ae6cd46a76b1b0e2d75e46eb03eed0b2ea5a501
urbanijose <urbanijosemaria6@gmail.com>
2025-08-23 10:37:56 -0300

    OrnitorrincoAzul completa segun el UML

--- src/mainornitorrinco/OrnitorrincoAzul.java
+++ src/mainornitorrinco/OrnitorrincoAzul.java
@@ -1,5 +1,6 @@
 package mainornitorrinco;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -15,5 +16,74 @@ import java.util.Scanner;
 * Julian Martin Esquiaga
 */
 public class OrnitorrincoAzul extends Castor {
+
+    private double propulsion;
+
+    public OrnitorrincoAzul(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void cargarDatos(Scanner leer) {
+        do { 
+            try { 
+                System.out.println("Ingrese la longitud de la cola (entre 0-6.8 cm)");
+                this.cola = leer.nextDouble();
+                leer.nextLine();
+                if (this.cola > 6.8 || this.cola < 0) {
+                    System.out.println("No puede ser mayor a 6.8 cm ni menor a 0 cm");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("El tipo de dato ingresado no es el correcto");
+                leer.nextLine();
+            }
+        } while (this.cola > 6.8 || this.cola < 0);
+
+        do {
+            try {
+                System.out.println("Ingrese la velocidad del ornitorrinco (maximo 8km/hora)");
+                this.velocidad = leer.nextDouble();
+                leer.nextLine();
+                if (this.velocidad > 8 || this.velocidad < 0) {
+                    System.out.println("No puede ser mayor a 8 ni menor a 0 km/hora");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("El tipo de dato ingresado no es el correcto");
+                leer.nextLine();
+            }
+        } while (this.velocidad > 8 || this.velocidad < 0);
+
+        do {
+            try {
+                System.out.println("Ingrese la propulsino del castor (entre 5-10 km/)");
+                this.propulsion = leer.nextDouble();
+                leer.nextLine();
+                if (this.propulsion > 10 || this.propulsion < 5) {
+                    System.out.println("No puede ser mayor a 10 ni menor a 5 km/s");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("El tipo de dato ingresado no es el correcto");
+                leer.nextLine();
+            }
+        } while (this.propulsion > 10 || this.propulsion < 5);
+    }
     
+    @Override
+    public void nadar() {
+        double velocidadFinal = this.velocidad + this.propulsion;
+        System.out.println(getNombre() + " nada a " + velocidadFinal + "km/hora");
+    }
+
+    public double getPropulsion() {
+        return propulsion;
+    }
+
+    public void setPropulsion(double propulsion) {
+        this.propulsion = propulsion;
+    }
+
+    @Override
+    public String toString() {
+        return "[OrnitorrincoAzul]" + " Nombre: " + nombre + " / Velocidad: " + velocidad + " / Propulsion: " + propulsion;
+    }
 }
