a7b076ff128975634c9146de3e98153035c0e73b
cristianb1983 <crisbenegas83@gmail.com>
2025-08-25 06:20:19 -0300

    Update OrnitorrincoVerde.java

--- src/mainornitorrinco/OrnitorrincoVerde.java
+++ src/mainornitorrinco/OrnitorrincoVerde.java
@@ -16,5 +16,32 @@ import java.util.Scanner;
 */
 
 public class OrnitorrincoVerde extends Castor implements Pata {
+      private String nombre;    
+    Scanner leer = new Scanner(System.in);
+    
+    public OrniVerde(String nombre, double cola, double velocidad) {
+        super(cola, velocidad);
+        this.nombre=nombre;
+    }
+    
+    @Override
+    public void tocarOrgano() {
+        System.out.println("Do-Re-Mi");
+        System.out.println("Presione enter para continuar...");
+        leer.nextLine();
+        System.out.println("Fa-Sol-La-Si");
+    }
+
+   public void tocarGuitarorgan(){
+    super.tocarGuitarra();
+    tocarOrgano();
+       System.out.println("cuac, cuaac..!");
+}
+
+    @Override
+    public String toString() {
+        return "OrniVerde{" + "nombre=" + nombre + '}';
+    }
+
     
 }
